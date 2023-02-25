97b06aeec657258aeee37f6ea0e921c5dc0abd3d
Victor Blanco <12vblanco@gmail.com>
2023-02-25 18:33:36 +0700

    menus on mobile

--- src/components/navbar/OurMenu.js
+++ src/components/navbar/OurMenu.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import { AiOutlineClose } from "react-icons/ai";
 import styled from "styled-components";
 import { menus } from "./Menus";
