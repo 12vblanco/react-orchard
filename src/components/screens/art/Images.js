d2ce3f655366ce3ab88a610ce0dae19e234cc8a8
Victor Blanco <12vblanco@gmail.com>
2023-04-15 07:15:53 +0800

    artNew

--- src/components/screens/art/Images.js
+++ src/components/screens/art/Images.js
@@ -1,5 +1,5 @@
-import current1 from "../../../images/art/newEx/unn1.jpg";
-import current3 from "../../../images/art/newEx/unn2.jpg";
-import current2 from "../../../images/art/newEx/unn3.jpg";
+import current1 from "../../../images/art/newEx/unn1.png";
+import current3 from "../../../images/art/newEx/unn2.png";
+import current2 from "../../../images/art/newEx/unn3.png";
 
 export const images = [{ src: current1 }, { src: current2 }, { src: current3 }];
