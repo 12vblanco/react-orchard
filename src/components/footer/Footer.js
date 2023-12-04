7273d485b3ea6210b8da4b5399d4cc48d5117672
Victor Blanco <12vblanco@gmail.com>
2023-12-04 21:28:35 +0000

    Update Node.js to version 16

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 import light from "../../images/light.jpg";
 import FooterContact from "./FooterContact";
